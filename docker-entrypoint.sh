0c871fd94a9fb4a1f0ed2638c995b2dd3ae0ac9c
Eloy Coto <eloy.coto@gmail.com>
2018-12-26 12:37:32 +0100

    Added DNSSEC option.
    To enable DNSSEC option for the given domain for testing pourposes.
    
    If the environment variable DNSSEC_DOMAIN is in place, keys will be
    created on /etc/bind/keys
    
    example zone details:
    
    ```
    zone "cilium.test" {
            type master;
            file "/etc/bind/db.cilium.test";
            auto-dnssec maintain;
            inline-signing yes;
            key-directory "/etc/bind/keys";
    };
    ```
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
 
-LOGGING=$(cat <<-EOF
+CONFIG=$(cat <<-EOF
+options {
+        directory "/var/cache/bind";
+        dnssec-validation auto;
+        auth-nxdomain no;    # conform to RFC1035
+        listen-on-v6 { any; };
+};
+
 logging {
     category default { default_stderr; };
     category queries { default_stderr; };
 };
 EOF
 )
-echo $LOGGING >> /etc/bind/named.conf.options
 
+chmod 777 /etc/bind
+echo -n "$CONFIG" > /etc/bind/named.conf.options
 cp -rfv /data/* /etc/bind/ || true
+
+if [ -n "${DNSSEC_DOMAIN}" ]; then
+    echo "Creating certificates for '${DNSSEC_DOMAIN}'"
+
+    mkdir -p -m777 /etc/bind/keys
+    cd /etc/bind/keys
+
+    dnssec-keygen ${DNSSEC_DOMAIN}
+    dnssec-keygen -fk ${DNSSEC_DOMAIN}
+    chown bind /etc/bind/keys/*
+fi
 
 named-checkconf /etc/bind/named.conf
 /usr/sbin/named -d 8 -c /etc/bind/named.conf -u bind -f
